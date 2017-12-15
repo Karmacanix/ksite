79cb45aa42039de46f9ebe5d646438179207512c
Karmacanix <gitmonitor@karmacanix.co.nz>
2017-12-16 09:43:15 +1300

    Updated Django for database

--- ksite/settings/production.py
+++ ksite/settings/production.py
@@ -4,9 +4,20 @@ from .base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['serene-bayou-31914.herokuapp.com']
 
 # need to call environment variables in this file
 # database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # secret key
 
